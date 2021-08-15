73d28b898da8a5b38245060caf68e4b7ce7f4d4a
sinan14 <sinanpmna7@gmail.com>
2021-08-15 17:54:59 +0530

    second

--- calc.js
+++ calc.js
@@ -1,12 +1,13 @@
-let num1String = "";
-let num2String = "";
+let firstNumString = "";
+let secondNumString = "";
 let allButtons = document.querySelectorAll("button");
-let operators = document.querySelectorAll("button.operators");
-const nums = document.querySelectorAll("button.numbers");
+// let operators = document.querySelectorAll("button.operators");
+const numberButtons = document.querySelectorAll("button.numbers");
 
 for (let button of allButtons) {
   button.addEventListener("click", function () {
     var checkOperator;
+    
     (function isOperator() {
       if (
         button.innerText == "/" ||
@@ -15,96 +16,65 @@ for (let button of allButtons) {
         button.innerText == "+"
       ) {
         checkOperator = true;
-        console.log(checkOperator);
+        // console.log(checkOperator);
       } else {
         checkOperator = false;
         console.log(checkOperator);
       }
     })();
 
-    if (checkOperator) {
-      for (let num of nums) {
-        num.addEventListener("click", function () {
-          let num2 = document.querySelectorAll("input")[2];
-          num2String += parseInt(num.innerText);
-          num2.value = parseInt(num2String);
-          console.log(num2.value);
-          console.log('num2')
+    if (checkOperator === false) {
+      for (let numberButton of numberButtons) {
+        numberButton.addEventListener("click", function () {
+          let firstNum = document.querySelectorAll("input")[0];
+          firstNumString += parseInt(numberButton.innerText);
+          firstNum.value = parseInt(firstNumString);
+          // console.log(firstNum.value);
         });
       }
-    } else if(checkOperator === false) {
-      for (let num of nums) {
-        num.addEventListener("click", function () {
-          let num1 = document.querySelectorAll("input")[0];
-          num1String += parseInt(num.innerText);
-          num1.value = parseInt(num1String);
-          console.log(num1.value);
-          console.log("ok");
+    } else {
+      for (let numberButton of numberButtons) {
+        numberButton.addEventListener("click", function () {
+          let secondNum = document.querySelectorAll("input")[2];
+          secondNumString += parseInt(numberButton.innerText);
+          secondNum.value = parseInt(secondNumString);
+          // console.log(secondNum.value);
         });
       }
     }
   });
 }
 
-const buttonEqual = document.querySelectorAll("button")[14];
-const buttonReset = document.querySelectorAll("button")[15];
-buttonEqual.addEventListener("click", calculate);
+//*********************** don't touch ************************************/
+//*************************it is working */
+// const buttonEqual = document.querySelectorAll("button")[14];
+// const buttonReset = document.querySelectorAll("button")[15];
+// buttonEqual.addEventListener("click", calculate);
 
-var result = "";
-function calculate() {
-  let num1r = document.querySelectorAll("input")[0].value;
-  let operator = document.querySelectorAll("input")[1];
-  let num2r = document.querySelectorAll("input")[2].value;
-  let resultBox = document.querySelectorAll("input")[3];
-  let num1 = parseInt(num1r);
-  let num2 = parseInt(num2r);
-  console.log(num1, num2, operator.value, num1r);
-  if (operator.value == "+") {
-    result = num1 + num2;
-    resultBox.value = result;
-  } else if (operator.value == "-") {
-    result = num1 - num2;
-    resultBox.value = result;
-  } else if (operator.value == "*") {
-    result = num1 * num2;
-    resultBox.value = result;
-  } else if (operator.value == "/") {
-    result = num1 / num2;
-    resultBox.value = result;
-  } else {
-    result = "syntax error";
-    alert("wrong input");
-  }
-}
+// var result = "";
+// function calculate() {
+//   let num1r = document.querySelectorAll("input")[0].value;
+//   let operator = document.querySelectorAll("input")[1];
+//   let num2r = document.querySelectorAll("input")[2].value;
+//   let resultBox = document.querySelectorAll("input")[3];
+//   let num1 = parseInt(num1r);
+//   let secondNum = parseInt(num2r);
 
-// const button1 = document.querySelectorAll("button")[0];
-// const button2 = document.querySelectorAll("button")[1];
-// const button3 = document.querySelectorAll("button")[2];
-// const button4 = document.querySelectorAll("button")[3];
-// const button5 = document.querySelectorAll("button")[4];
-// const button6 = document.querySelectorAll("button")[5];
-// const button7 = document.querySelectorAll("button")[6];
-// const button8 = document.querySelectorAll("button")[7];
-// const button9 = document.querySelectorAll("button")[8];
-// const button0 = document.querySelectorAll("button")[9];
-// const buttonM = document.querySelectorAll("button")[10];
-// const buttonD = document.querySelectorAll("button")[11];
-// const buttonS = document.querySelectorAll("button")[12];
-// const buttonA = document.querySelectorAll("button")[13];
-
-//   if (
-//     button.value == "*" ||
-//     button.value == "/" ||
-//     button.value == "-" ||
-//     button.value == "+"
-//   ) {
-//     for (let num of nums) {
-//       num.classList.add("text-danger");
-//       num.addEventListener("click", function () {
-//         let num1 = document.querySelectorAll("input")[0];
-//         num1String += parseInt(num.innerText);
-//         num1.value = parseInt(num1String);
-//         console.log(num1.value);
-//       });
-//     }
+//   console.log(num1, secondNum, operator.value, num1r);
+//   if (operator.value == "+") {
+//     result = num1 + secondNum;
+//     resultBox.value = result;
+//   } else if (operator.value == "-") {
+//     result = num1 - secondNum;
+//     resultBox.value = result;
+//   } else if (operator.value == "*") {
+//     result = num1 * secondNum;
+//     resultBox.value = result;
+//   } else if (operator.value == "/") {
+//     result = num1 / secondNum;
+//     resultBox.value = result;
+//   } else {
+//     result = "syntax error";
+//     alert("wrong input");
 //   }
+// }
